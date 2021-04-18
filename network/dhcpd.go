d26dc7e25f0be2deb4898923b7fc810b68b56d9d
Jaci Brunning <jaci.brunning@gmail.com>
2021-04-18 13:06:14 +0000

    Add the ability to automatically update /etc/dhcp/dhcpd.conf

--- network/dhcpd.go
+++ network/dhcpd.go
@@ -3,7 +3,9 @@ package network
 import (
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/JaciBrunning/jms/util"
@@ -36,15 +38,20 @@ func ReloadDHCPDService() error {
 	defer conn.Close()
 	channel := make(chan string)
 
-	logger.Info("[DHCP] Stopping DHCP Service...")
+	logger.Info("Stopping DHCP Service...")
 	_, err = conn.StopUnit("isc-dhcp-server.service", "replace", channel)
 	if err != nil {
 		return err
 	}
 	_ = <-channel // Wait for reload to be done
 
-	// TODO: Make this an option
-	// logger.Info("[DHCP] Flushing DHCP Caches...")
+	maybeUpdateDHCPdConf()
+
+	// TODO: Make this an option. Should not be required; enabling may
+	// de-lease FTA equipment. If systems contain old leases, disconnect and
+	// reconnect them physically as some OSs (such as windows) require a physical
+	// reconnection to send a DHCPDISCOVER instead of a DHCPREQUEST
+	// logger.Info("Flushing DHCP Caches...")
 	// err = os.Remove("/var/lib/dhcp/dhcpd.leases")
 	// if err != nil {
 	// 	return err
@@ -54,7 +61,7 @@ func ReloadDHCPDService() error {
 	// 	return err
 	// }
 
-	logger.Info("[DHCP] Starting DHCP Service...")
+	logger.Info("Starting DHCP Service...")
 	_, err = conn.ReloadOrRestartUnit("isc-dhcp-server.service", "replace", channel)
 	if err != nil {
 		return err
@@ -62,14 +69,33 @@ func ReloadDHCPDService() error {
 	_ = <-channel               // Wait for reload to be done
 	time.Sleep(2 * time.Second) // Wait for the service to know what's going on
 
-	logger.Info("[DHCP] Checking if DHCP Service is active...")
+	logger.Info("Checking if DHCP Service is active...")
 	props, err := conn.GetUnitProperties("isc-dhcp-server.service")
 	if err != nil {
 		return err
 	}
 	if props["ActiveState"] != "active" {
-		return fmt.Errorf("[DHCP] DHCP service failed to start")
+		return fmt.Errorf("DHCP service failed to start")
 	}
-	logger.Info("[DHCP] DHCP Service is up!")
+	logger.Info("DHCP Service is up!")
 	return nil
+}
+
+func maybeUpdateDHCPdConf() {
+	content, err := ioutil.ReadFile("/etc/dhcp/dhcpd.conf")
+	if err != nil {
+		logger.Warnf("Cannot check /etc/dhcp/dhcpd.conf - %s", err)
+		return
+	}
+
+	include_string := "include \"/etc/dhcp/jms-dhcp.conf\";"
+	if !strings.Contains(string(content), include_string) {
+		f, err := os.OpenFile("/etc/dhcp/dhcpd.conf", os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			logger.Warnf("Cannot append to /etc/dhcp/dhcpd.conf - %s", err)
+		} else {
+			fmt.Fprintf(f, "\n# Automatically added by JMS\n%s\n", include_string)
+			logger.Info("JMS added to /etc/dhcp/dhcpd.conf (first-run)")
+		}
+	}
 }
