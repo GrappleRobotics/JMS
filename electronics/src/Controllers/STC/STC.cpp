6f2fb69acecc0a621cf10739d21ea234de4a5b40
Connor <connorbuchel@gmail.com>
2021-08-03 23:49:14 +0800

    Tested TCP networking (basic)

--- electronics/src/Controllers/STC/STC.cpp
+++ electronics/src/Controllers/STC/STC.cpp
@@ -1,33 +1,40 @@
 #include "STC.h"
 
 const char *ECHO_SERVER_ADDRESS = "192.168.178.125";
-const int ECHO_SERVER_PORT = 9999;
+const int ECHO_SERVER_PORT = 5800;
+EthernetInterface eth;
 
 int STC_Controller::start(int argc, char const *argv[]) {
 
 	Handle(
 		setRUNNING(true);
-		char buf[256] = "Hello world";
+		char buf[1024] = "Hello world";
 
 		// Network testing
-		EthernetInterface eth;
 		eth.connect();
 
 		SocketAddress a;
-		a.set_ip_address(ECHO_SERVER_ADDRESS);
-		a.set_port(ECHO_SERVER_PORT);
 		eth.get_ip_address(&a);
-		
-		std::cout << "Client IP Addr: " << a.get_ip_address() << std::endl;
-		std::cout << "Client Port: " << a.get_port() << std::endl;
 
 		// connect to server
 		TCPSocket socket;
-		while (socket.connect(a) < 0) {
-			std::cout << "Unable to connect to " << ECHO_SERVER_ADDRESS << " " << ECHO_SERVER_PORT << std::endl;
+		
+		if (socket.open(&eth) < 0) {
+			std::cout << "Failed to open TCP socket" << std::endl;
 		}
 
-		std::cout << "Connected to server at: " << ECHO_SERVER_ADDRESS << " " << ECHO_SERVER_PORT << std::endl;
+		eth.gethostbyname("ifconfig.io", &a);
+		a.set_ip_address(ECHO_SERVER_ADDRESS);
+		a.set_port(ECHO_SERVER_PORT);
+
+		std::cout << "Client IP Addr: " << a.get_ip_address() << std::endl;
+		std::cout << "Client Port: " << a.get_port() << std::endl;
+
+		while (socket.connect(a) < 0) {
+			std::cout << "Unable to connect to " << ECHO_SERVER_ADDRESS << ":" << ECHO_SERVER_PORT << std::endl;
+		}
+
+		std::cout << "Connected to server at: " << ECHO_SERVER_ADDRESS << ":" << ECHO_SERVER_PORT << std::endl;
 
 		socket.send(buf, sizeof(buf) - 1);
 
