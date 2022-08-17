a633e7e2ede75c1d346bab55d067a226dcbe7bc9
Jaci Brunning <jaci.brunning@gmail.com>
2022-08-18 01:17:27 +0800

    Make audience display fade

--- jms-frontend/src/audience/Audience.tsx
+++ jms-frontend/src/audience/Audience.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { WebsocketComponent } from "support/ws-component";
 import { AudienceDisplay, EventDetails } from "ws-schema";
 import AudienceSceneAllianceSelection from "./AllianceSelection";
@@ -27,18 +28,19 @@ export default class Audience extends WebsocketComponent<{}, AudienceState> {
     const { scene, event_details } = this.state;
     
     if (scene != null && event_details != null) {
-      
       // Render all, but only one is active at a time
       // This way, each element keeps its own listen  so we don't have to wait for websocket updates after mount
       return <div className="audience-root" style={ { "--chroma-key-colour": event_details.av_chroma_key, "--event-colour": event_details.av_event_colour } as React.CSSProperties }>
-        <AudienceSceneField details={event_details} props={scene.scene === "Field" ? {} : undefined} />
-        <AudienceSceneCustomMessage details={event_details} props={scene.scene === "CustomMessage" ? { msg: scene.params } : undefined} />
-        <AudienceSceneAward details={event_details} props={scene.scene === "Award" ? scene.params : undefined} />
-        <AudienceSceneMatchPreview details={event_details} props={scene.scene === "MatchPreview" ? {} : undefined} />
-        <AudienceSceneMatchPlay details={event_details} props={scene.scene === "MatchPlay" ? {} : undefined} />
-        <AudienceSceneMatchResults details={event_details} props={scene.scene === "MatchResults" ? { match: scene.params } : undefined} />
-        <AudienceSceneAllianceSelection details={event_details} props={scene.scene === "AllianceSelection" ? {} : undefined} />
-        <AudienceScenePlayoffBracket details={event_details} props={scene.scene === "PlayoffBracket" ? {} : undefined} />
+        {/* <TransitionGroup> */}
+          <AudienceSceneField details={event_details} props={scene.scene === "Field" ? {} : undefined} />
+          <AudienceSceneCustomMessage details={event_details} props={scene.scene === "CustomMessage" ? { msg: scene.params } : undefined} />
+          <AudienceSceneAward details={event_details} props={scene.scene === "Award" ? scene.params : undefined} />
+          <AudienceSceneMatchPreview details={event_details} props={scene.scene === "MatchPreview" ? {} : undefined} />
+          <AudienceSceneMatchPlay details={event_details} props={scene.scene === "MatchPlay" ? {} : undefined} />
+          <AudienceSceneMatchResults details={event_details} props={scene.scene === "MatchResults" ? { match: scene.params } : undefined} />
+          <AudienceSceneAllianceSelection details={event_details} props={scene.scene === "AllianceSelection" ? {} : undefined} />
+          <AudienceScenePlayoffBracket details={event_details} props={scene.scene === "PlayoffBracket" ? {} : undefined} />
+        {/* </TransitionGroup> */}
     </div>
     } else {
       return <React.Fragment />
